ddeea7f86bed3d4e66e90ff5e239b5b7f60bd0bf
Mizab <mizabmansari@gmail.com>
2024-01-15 17:11:11 +0530

    Second Phase for Custom Creeper

--- src/CustomCreeper/Private/CustomCreeperComponents.ts
+++ src/CustomCreeper/Private/CustomCreeperComponents.ts
@@ -1,24 +1,93 @@
-import { Coordinates, MCFunction, summon } from "sandstone";
+import {
+  Coordinates,
+  Data,
+  MCFunction,
+  NBT,
+  Selector,
+  execute,
+  summon,
+} from "sandstone";
+import { self } from "../../Tick";
 
 /**
- * Spawns a custom creeper with the given name, tag, and coordinates.
+ * Generates a custom creeper spawn file for the given name, tag, coordinates, explosion radius, initial fuse, and dynamic flag.
  *
  * @param {string} name - The name of the custom creeper.
  * @param {string} tag - The tag of the custom creeper.
- * @param {Coordinates} coords - The coordinates where the custom creeper should be spawned.
+ * @param {Coordinates} coords - The coordinates where the creeper will be spawned.
+ * @param {number} explosionRadius - The explosion radius of the creeper or how much power will it contain. Default is 3.
+ * @param {number} initialFuse - The initial fuse of the creeper. Default is 30.
+ * @param {boolean} isDynamic - A flag indicating whether the creeper is dynamic or static. Default is false.
  */
-export const SpawnFileForCustomCreeper = (
+const SpawnFileForCustomCreeper = (
   name: string,
   tag: string,
-  coords: Coordinates
+  coords: Coordinates,
+  explosionRadius: number = 3,
+  initialFuse: number = 30,
+  isDynamic: boolean = false
 ) => {
   MCFunction(
-    `spawn_custom_creeper/spawn_${name.replace(" ", "_").toLocaleLowerCase()}`,
+    `custom_creeper/spawn/spawn_${name.replace(" ", "_").toLocaleLowerCase()}`,
     () => {
       summon("minecraft:creeper", coords, {
         CustomName: `{"text": "${name}"}`,
-        Tags: ["custom_creeper", tag],
+        Tags: [
+          "custom_creeper",
+          tag,
+          isDynamic ? "dynamic_creeper" : "static_creeper",
+        ],
+        ExplosionRadius: NBT.byte(explosionRadius),
+        Fuse: initialFuse,
       });
     }
   );
+};
+
+/**
+ * Creates a custom creeper entity with the specified name, tag, coordinates, explosion radius, initial fuse, and final fuse.
+ * If a creeper handler function is provided, it will be called when the creeper entity is executed.
+ *
+ * @param {string} name - The name of the custom creeper entity.
+ * @param {string} tag - The tag of the custom creeper entity.
+ * @param {Coordinates} coords - The coordinates of the custom creeper entity.
+ * @param {number} [explosionRadius=3] - The explosion radius of the custom creeper entity.
+ * @param {number} [initialFuse=30] - The initial fuse of the custom creeper entity.
+ * @param {number} [finalFuse=1] - The final fuse which will decide when will it explodes.
+ * @param {() => void | null} creeperHandlerFunction - The function to be handles the exploding mechanics of the creeper.
+ */
+export const createCustomCreeper = (
+  name: string,
+  tag: string,
+  coords: Coordinates,
+  explosionRadius: number = 3,
+  initialFuse: number = 30,
+  finalFuse: number = 1,
+  creeperHandlerFunction: () => void | null
+) => {
+  // Call the spawnFileFunction
+  SpawnFileForCustomCreeper(
+    name,
+    tag,
+    coords,
+    explosionRadius,
+    initialFuse,
+    creeperHandlerFunction ? true : false
+  );
+
+  // Call the function if it is available
+  if (creeperHandlerFunction) {
+    // Execute the function of the behalf og this entity
+    execute
+      .as(
+        Selector("@s", {
+          tag: [tag],
+          scores: { creeper_fuse_obj: [Infinity, finalFuse] },
+        })
+      )
+      .at(self)
+      .run(() => {
+        creeperHandlerFunction();
+      });
+  }
 };
--- src/CustomCreeper/Tick.ts
+++ src/CustomCreeper/Tick.ts
@@ -1,12 +1,53 @@
-import { MCFunction, rel } from "sandstone";
-import { SpawnFileForCustomCreeper } from "./Private/CustomCreeperComponents";
+import {
+  Data,
+  MCFunction,
+  Objective,
+  ObjectiveInstance,
+  Selector,
+  execute,
+  rel,
+  say,
+  scoreboard,
+} from "sandstone";
+import { createCustomCreeper } from "./Private/CustomCreeperComponents";
 
-SpawnFileForCustomCreeper("Instant Creeper", "instant_creeper", rel(0, 0, 0));
+const self = Selector("@s");
 
+/* LOAD */
+var fuseObj: ObjectiveInstance<string> = Objective.create(
+  "creeper_fuse_obj",
+  "dummy"
+);
+
+/* TICK */
 MCFunction(
   "custom_creeper/tick",
   () => {
-    // Tick
+    // Run the creeper handler every tick and create a creeper file once
+    //! This function will create a file regardless of the condition status, but it will run the handler function on only "dynamic" creeper
+    execute
+      .as(Selector("@e", { type: "minecraft:creeper", tag: "dynamic" }))
+      .at(self)
+      .run(() => {
+        // Detecting if the creeper is ignited and storing its Fuse in a scoreboard
+        execute.store.result
+          .score(self, "creeper_fuse_obj")
+          // @ts-ignore
+          .run.data.get.entity(self, "fuse");
+
+        /* Start creating custom creeper from here */
+        createCustomCreeper(
+          "Instant Creeper",
+          "instant_creeper",
+          rel(0, 0, 0),
+          5,
+          1,
+          1,
+          () => {
+            say("Hello");
+          }
+        );
+      });
   },
   {
     runEachTick: true,
